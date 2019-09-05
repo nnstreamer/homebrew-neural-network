690e4e89d952f7806875786c1a96c294e27f9b01
Wook Song <wook16.song@samsung.com>
2019-09-05 22:17:01 +0900

    [Formula/SSAT] Import a commit which is 60a2e40
    This patch imports a commit (60a2e40) from the downstream repository in
    nnsuite.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

--- Formula/ssat.rb
+++ Formula/ssat.rb
@@ -1,9 +1,9 @@
 class Ssat < Formula
   desc "Shell Script Automated Tester (unit testing executable files)"
   homepage "https://github.com/myungjoo/SSAT"
-  url "https://github.com/nnsuite/SSAT/archive/fa636a9882e920f9cb1dbc2e4e96082b86f4776f.zip"
+  url "https://github.com/nnsuite/SSAT/archive/60a2e405bdd466dcc839c90a4075a5a42c6302fe.zip"
   version "1.0.0"
-  sha256 "ff0acc26aa224ae6c78e12347b78478f38452af440b0932b2fe8145912e02ce7"
+  sha256 "2d5a6d777c20e8d795521d0f1d835ae2dfcebe5de57b28db486eb1f3adb541fe"
 
   def install
     system "ln","-s","ssat.sh", "ssat"
