e576aaab323429a1d1625b1e887fab3b784fea0d
Wook Song <wook16.song@samsung.com>
2019-10-26 00:00:36 +0900

    [Formula/NNStreamer] Enable build-time tests
    This patch enables build-time tests by default.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

--- Formula/nnstreamer.rb
+++ Formula/nnstreamer.rb
@@ -1,16 +1,16 @@
 class Nnstreamer < Formula
   desc "Neural Network (NN) Streamer, Stream Processing Paradigm for Neural Network Apps/Devices."
   homepage "https://github.com/nnsuite/nnstreamer"
-  url "https://github.com/nnsuite/nnstreamer/archive/d1b0cf4625c1efa309d696c9c6100b242b63d3d8.tar.gz"
+  url "https://github.com/nnsuite/nnstreamer/archive/c110844c31bd0fb4eb0a8cf6ac8bf969c81db397.tar.gz"
   version "1.0.0"
-  sha256 "f6f267a4e643d881bce375dcec6988047cdf2bb301e1b84dbff0eb95024a5115"  
-
+  sha256 "d4397a77a18fc0d881b2aff4fdf68e251ccea7ca84d8a765c52eee0df1cd7e76"
   depends_on :macos
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "cmake" => :build
   depends_on "googletest" => :build
+  depends_on "ssat" => :build
   depends_on "protobuf"
   depends_on "libtensorflow"
   depends_on "libffi"
@@ -22,8 +22,26 @@ class Nnstreamer < Formula
 
   def install
     system "rm", "-rf", "build"
-    system "meson", "build", "--prefix=#{prefix}", "--sysconfdir=#{prefix}/etc", "-Denable-tensorflow-lite=false", "-Denable-pytorch=false", "-Denable-caffe2=false"
-    system "ninja", "-C", "build", "install"
+    system "meson", "build", "--prefix=#{prefix}", "--sysconfdir=#{prefix}/etc",\
+        "-Denable-tensorflow-lite=false", "-Denable-pytorch=false", \
+        "-Denable-caffe2=false"
+    system "ninja", "-C", "build"
+    # Before installation, run build-time tests
+    cd "build" do
+      ENV["GST_PLUGIN_PATH"] = "#{Dir.getwd}/gst/nnstreamer"
+      ENV["NNSTREAMER_CONF"] = "#{Dir.getwd}/nnstreamer-test.ini"
+      ENV["NNSTREAMER_FILTERS"] = "#{Dir.getwd}/ext/nnstreamer/tensor_filter"
+      ENV["NNSTREAMER_DECODERS"] = "#{Dir.getwd}/ext/nnstreamer/tensor_decoder"
+      system "./tests/unittest_common"
+      system "./tests/unittest_sink", "-j", "5000", "-f", "10"
+      system "./tests/unittest_plugins"
+    end
+    cd "tests" do
+      # FIXME: Several tests are going to be failed
+      system "ssat"
+    end
+    cd "build" do
+      system "ninja", "install"
+    end
   end
-
 end
